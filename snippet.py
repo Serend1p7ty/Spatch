8fec6fae63f38bc49c499e57aa5e31a5a3300706
1748415344@qq.com <serend1p7ty@ubuntu24.04>
2025-09-25 09:16:03 +0800

    添加生成模板exp功能，添加给patch文件增加可执行权限功能

---
+++ snippet.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from pwn import *
+context(log_level='debug',arch='amd64')
+file = ''
+elf = ELF(file)
+libc = ELF('')
+rop = ROP(file)
+ip,port = '',''
+local = 1
+if local:
+    p = process(file)
+else:
+    p = remote(ip,port)
+
+
+sla = lambda delim,data         :   p.sendlineafter(delim,data)
+sl  = lambda data               :   p.sendline(data)
+sa  = lambda delim,data         :   p.sendafter(delim,data)
+ita = lambda                    :   p.interactive()
+ra  = lambda                    :   p.recvall()
+ru  = lambda delim,drop=True    :   p.recvuntil(delim,drop)
+rv  = lambda numb               :   p.recv(numb)
+lg  = lambda msg                :   log.success(msg)
+def dbg(addr=0):
+    if addr != 0:
+        script = f'b *$rebase({str(addr)})'
+        attach(p,script)
+    else:
+        attach(p)
+    pause()
+
+
+
+leaked = ''
+libc_base = 0
